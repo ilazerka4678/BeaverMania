55eeed6d58af7699adcc90bc1c81de6492a4669e
BT_4N2_02 <ilazerka4678@bths.edu>
2023-05-02 11:50:09 -0400

    classes added

--- src/Beaver.java
+++ src/Beaver.java
@@ -1,2 +1,12 @@
 public class Beaver {
+    private double weight;
+    private int furQuality;
+    private String beaverType;
+
+    public Beaver(double w, int f, String b){
+        weight = w;
+        furQuality = f;
+        beaverType = b;
+    }
+
 }
