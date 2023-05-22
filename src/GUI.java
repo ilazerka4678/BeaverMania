58f3cf741f70b8e9f66766127614dc8cc5bb668e
BT_4N2_02 <ilazerka4678@bths.edu>
2023-05-22 12:12:23 -0400

    partial GUI

--- src/GUI.java
+++ src/GUI.java
@@ -9,24 +9,36 @@ public class GUI extends JFrame implements ActionListener {
     private JButton startButton;
     private JPanel mainPanel;
     private JTextArea userInfo;
+    private JButton quitButton;
+    private JButton button1;
+    private JButton button2;
 
-    public GUI(){
+    public GUI() {
 
-        setSize(600,600);
-        setLocation(400,200);
+        setSize(600, 600);
+        setLocation(400, 200);
         setContentPane(mainPanel);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
         userInfo.setEditable(false);
         startButton.addActionListener(this);
+        quitButton.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        JButton button = (JButton)e.getSource();
-        if (button.getText().equals("Start") && !userName.getText().equals("")){
-            String userText = "Welcome to Beaver Hunt, " + userName.getText() + "!";
-            userInfo.setText(userText);
+        JButton button = (JButton) e.getSource();
+        if (button.getText().equals("Start")) {
+            if (!userName.getText().equals("")) {
+                String userText = "Welcome to Beaver Hunt, " + userName.getText() + "!";
+                userInfo.setText(userText);
+            }
+            else{
+                userInfo.setText("You need to set your username first.");
+            }
+        }
+        if (button.getText().equals("Quit")){
+            this.dispose();
         }
     }
 }
