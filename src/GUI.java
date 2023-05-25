d099c4cdabaf4ba4b239999f480b79f127cc176d
BT_4N2_02 <ilazerka4678@bths.edu>
2023-05-25 12:09:39 -0400

    beaver visuals

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class GUI extends JFrame implements ActionListener{
 
@@ -13,11 +17,25 @@ public class GUI extends JFrame implements ActionListener{
     private JButton feedBeaversButton;
     private JButton shaveBeaversButton;
     private JButton buyBeaverButton;
+    private JLabel beaverSlot1;
+    private JLabel beaverSlot2;
+    private JLabel beaverSlot3;
+    private JLabel beaverSlot4;
+    private JLabel beaverSlot5;
+    private JLabel beaverSlot6;
+    private JLabel beaverSlot7;
+    private JLabel beaverSlot8;
+    private JLabel beaverSlot9;
+    private JLabel beaverSlot10;
+    private JLabel beaverSlot11;
+    private JLabel beaverSlot12;
     private Player player;
     private PlayersData playerData;
+    private int slot;
 
 
     public GUI() {
+        slot = 1;
         playerData = new PlayersData();
         setSize(600, 500);
         setLocation(700, 200);
@@ -31,6 +49,17 @@ public class GUI extends JFrame implements ActionListener{
         feedBeaversButton.addActionListener(this);
         shaveBeaversButton.addActionListener(this);
         buyBeaverButton.addActionListener(this);
+
+    }
+    public ImageIcon getIcon(String filename) {
+        try {
+            BufferedImage bi = ImageIO.read(new File(filename));
+            return new ImageIcon(bi, "");
+        }
+        catch (Exception io) {
+            System.out.println("Unable to find image");
+            return null;
+        }
     }
 
     @Override
@@ -56,13 +85,54 @@ public class GUI extends JFrame implements ActionListener{
                 userInfo.setText("You need to set your username first.");
             }
         }
-        if (button.getText().equals("Buy Beaver")) {
+        if (button.getText().equals("Buy Beaver") && !(userName.getText().equals(""))) {
             if (player.addBeaver(new Beaver((int)(Math.random()*10),(int)(Math.random()*101),(int)(Math.random()*5))) != -1){
                 userText = "Bought a beaver!";
                 userText += "\n" + "It's a " + player.getBeaverList().get(player.getBeaverList().size()-1).getBeaverType() + "!!!";
                 userText += "\n" + "Gold: " + player.getGold();
                 userText += "\n" + "Next beaver will cost: " + player.getNextBeaverPrice();
                 userInfo.setText(userText);
+                ImageIcon icon = getIcon("C:\\Users\\BT_4N2_02\\Downloads\\Real-Beaver.png");
+                Image image = icon.getImage();
+                image = image.getScaledInstance(50,50, Image.SCALE_SMOOTH);
+                ImageIcon scaledIcon = new ImageIcon(image);
+                if (slot == 1) {
+                    beaverSlot1.setIcon(scaledIcon);
+                }
+                if (slot == 2) {
+                    beaverSlot2.setIcon(scaledIcon);
+                }
+                if (slot == 3){
+                    beaverSlot3.setIcon(scaledIcon);
+                }
+                if (slot == 4){
+                    beaverSlot4.setIcon(scaledIcon);
+                }
+                if (slot == 5){
+                    beaverSlot5.setIcon(scaledIcon);
+                }
+                if (slot == 6){
+                    beaverSlot6.setIcon(scaledIcon);
+                }
+                if (slot == 7){
+                    beaverSlot7.setIcon(scaledIcon);
+                }
+                if (slot == 8){
+                    beaverSlot8.setIcon(scaledIcon);
+                }
+                if (slot == 9){
+                    beaverSlot9.setIcon(scaledIcon);
+                }
+                if (slot == 10){
+                    beaverSlot10.setIcon(scaledIcon);
+                }
+                if (slot == 11){
+                    beaverSlot11.setIcon(scaledIcon);
+                }
+                if (slot == 12){
+                    beaverSlot12.setIcon(scaledIcon);
+                }
+                slot++;
             }
 
 
